f7f919ede0eec073e058f5f4d765ccc3d36be31d
Olga-max658 <stonesvips@gmail.com>
2024-12-17 01:36:36 +0500

    вывод заголовков

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cstdint>
 #include <string>
+#include <windows.h>
 
 using namespace std;
 
@@ -29,8 +30,40 @@ uint16_t header_flags;
 uint16_t header_size;
 };
 
-
 #pragma pack(pop)
+
+void readRar(vector<char>& rar_data)
+{
+    header_format* p_header = reinterpret_cast<header_format*>(&rar_data[7]);
+    int offset = 7 + int(p_header -> header_size);
+    while (true)
+    {
+        header_format* p_header = reinterpret_cast <header_format*> (&rar_data[offset]);
+        file_header* f_header = reinterpret_cast <file_header*> (&rar_data[offset+7]);
+        if (p_header -> header_type == 0x74)
+        {
+            uint16_t sizeHeader = f_header -> NameSize;
+            uint16_t offsetHeader = offset + sizeof (struct header_format) + sizeof (struct file_header);
+            if (offsetHeader + sizeHeader > rar_data.size())
+            {
+                break;
+            }
+            char* jumpCell = reinterpret_cast<char*>(&rar_data[offsetHeader]);
+            string utf8line(jumpCell, sizeHeader);
+            vector <wchar_t> utf16content(sizeHeader);
+            MultiByteToWideChar(CP_UTF8,0,utf8line.c_str(),sizeHeader,utf16content.data(),utf16content.size());
+            wstring FileName(utf16content.begin(),utf16content.end());
+            wcout << "Name file: " << FileName << endl;
+            offset = offset + p_header -> header_size + f_header -> PackSize;
+        }
+        else
+        {
+            offset += p_header->header_size;
+        }
+
+    }
+
+};
 
 
 int main()
@@ -48,8 +81,7 @@ int main()
     rar_file.seekg(0, ios::beg);
     vector<char> rar_data(fileSize, 0);
     rar_file.read(rar_data.data(), fileSize);
-
-
+    readRar(rar_data);
 
 return 0;
 }
